870c9cead20ba3773de81623285fa57e14aae7b0
Alekseywow <danilov20022@mail.ru>
2022-11-20 13:38:32 +0500

    added animation page + js

--- src/js/script.js
+++ src/js/script.js
@@ -102,4 +102,21 @@ $(document).ready(function(){
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
 
+
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 1600 ) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href=#up]").click(function(){
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+    });
+
+    new WOW().init();
+
   });
